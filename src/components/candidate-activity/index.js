b44ca30f16ecc2d4553c077dd50a5c2aed93ef54
omarjafor <omarjaforchy@gmail.com>
2024-07-10 21:53:07 +0600

    candidate activity page done

--- src/components/candidate-activity/index.js
+++ src/components/candidate-activity/index.js
@@ -1,17 +1,41 @@
 'use client'
 
-import { Tabs, TabsList } from "../ui/tabs";
+import CommonCard from "../common-card";
+import JobIcon from "../job-icon";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 
 const CandidateActivity = ({ jobList, jobApplicationList }) => {
-    
+    const uniqueStatus = [...new Set(jobApplicationList.map(jobItem => jobItem.status).flat(1))]
+    console.log(uniqueStatus);
     return (
         <div className="mx-auto max-w-7xl">
             <Tabs defaultValue="Applied" className="w-full">
                 <div className="flex items-center justify-between border-b pb-6 pt-24">
                     <h1 className="text-4xl font-bold tracking-tight text-gray-950">Your Activity</h1>
                     <TabsList>
-
+                        {
+                            uniqueStatus.map(status => <TabsTrigger value={status}>{status}</TabsTrigger>)
+                        }
                     </TabsList>
+                </div>
+                <div className="pb-24 pt-6">
+                    <div className="container mx-auto space-y-8">
+                        <div className="flex flex-col gap-8">
+                            {
+                                uniqueStatus.map((status) => (
+                                    <TabsContent value={status}>
+                                        {
+                                            jobList.filter((jobItem) => jobApplicationList.filter(jobApp => jobApp.status.indexOf(status) > -1).findIndex(filterItemByStatus => jobItem._id === filterItemByStatus.jobId) > -1).map(finalJobItem => <CommonCard
+                                            icon={<JobIcon/>}
+                                            title={finalJobItem?.title}
+                                            description={finalJobItem?.description || finalJobItem?.companyName}
+                                            />)
+                                        }
+                                    </TabsContent>
+                                ))
+                            }
+                        </div>
+                    </div>
                 </div>
             </Tabs>
         </div>
