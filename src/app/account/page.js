bda23d43cef84af7444137054629cab9a11e4bf1
omarjafor <omarjaforchy@gmail.com>
2024-07-12 22:02:20 +0600

    activity page filter option

---
+++ src/app/account/page.js
@@ -0,0 +1,10 @@
+import AccountInfo from "@/components/account-info";
+
+
+const Account = () => {
+    return (
+        <AccountInfo />
+    );
+};
+
+export default Account;
---
+++ src/components/account-info/index.js
@@ -0,0 +1,11 @@
+
+
+const AccountInfo = () => {
+    return (
+        <div>
+            Here is your Account Info
+        </div>
+    );
+};
+
+export default AccountInfo;
